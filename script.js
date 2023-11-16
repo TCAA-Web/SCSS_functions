b6437961b5b37c319aed402e011e1c396742ec7f
kasperfryd <kfry@techcollege.dk>
2023-11-16 08:38:02 +0100

    Update with theme switcher

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-let colorContainer = document.getElementById('colorContainer')
+let colorContainer = document.getElementById("colorContainer");
 
 // For loop to add create 50 div elements
 // and add a class 'rngColor' to each of them
-for (let i = 0; i < 50; i++){
-    let element = document.createElement('div')
-    element.classList.add('rngColor')
-    colorContainer.append(element)
+for (let i = 0; i < 50; i++) {
+  let element = document.createElement("div");
+  element.classList.add("rngColor");
+  colorContainer.append(element);
 }
+
+// Theme switching
+const btn = document.getElementById("themeBtn");
+const content = document.getElementById("content");
+
+function toggleTheme() {
+  btn.classList.toggle("dark");
+  btn.classList.toggle("light");
+  if (btn.innerText == "dark") {
+    btn.innerText = "light";
+  } else btn.innerText = "dark";
+
+  content.classList.toggle("dark");
+  content.classList.toggle("light");
+}
+
+btn.addEventListener("click", toggleTheme);
